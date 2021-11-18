7238046196c8a09a520fac1d6b9033cb7c44de77
jordaches <44157258+jordaches@users.noreply.github.com>
2021-11-18 15:09:02 +0200

    Init

---
+++ RToast/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace RToast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rb_short = new System.Windows.Forms.RadioButton();
+            this.rb_long = new System.Windows.Forms.RadioButton();
+            this.rb_stay = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Toast";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(13, 561);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pop Toast";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(276, 561);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Schedule Toast";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Heading";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 22);
+            this.textBox1.MaxLength = 90;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Sample Heading";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 91);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(2, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Line 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 49);
+            this.textBox2.MaxLength = 170;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Sample Text";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(22, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Content";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.rb_short);
+            this.panel2.Controls.Add(this.rb_long);
+            this.panel2.Controls.Add(this.rb_stay);
+            this.panel2.Location = new System.Drawing.Point(13, 175);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(353, 111);
+            this.panel2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(21, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(247, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "How long will the notification remain on the screen.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // rb_short
+            // 
+            this.rb_short.AutoSize = true;
+            this.rb_short.Checked = true;
+            this.rb_short.Location = new System.Drawing.Point(11, 79);
+            this.rb_short.Name = "rb_short";
+            this.rb_short.Size = new System.Drawing.Size(50, 17);
+            this.rb_short.TabIndex = 11;
+            this.rb_short.TabStop = true;
+            this.rb_short.Text = "Short";
+            this.rb_short.UseVisualStyleBackColor = true;
+            // 
+            // rb_long
+            // 
+            this.rb_long.AutoSize = true;
+            this.rb_long.Location = new System.Drawing.Point(11, 56);
+            this.rb_long.Name = "rb_long";
+            this.rb_long.Size = new System.Drawing.Size(49, 17);
+            this.rb_long.TabIndex = 10;
+            this.rb_long.Text = "Long";
+            this.rb_long.UseVisualStyleBackColor = true;
+            // 
+            // rb_stay
+            // 
+            this.rb_stay.AutoSize = true;
+            this.rb_stay.Location = new System.Drawing.Point(11, 33);
+            this.rb_stay.Name = "rb_stay";
+            this.rb_stay.Size = new System.Drawing.Size(46, 17);
+            this.rb_stay.TabIndex = 9;
+            this.rb_stay.Text = "Stay";
+            this.rb_stay.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(21, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Notification Duration";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "20",
+            "30",
+            "60",
+            "120",
+            "300"});
+            this.comboBox1.Location = new System.Drawing.Point(209, 563);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(294, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(294, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 589);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RToast";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rb_stay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rb_short;
+        private System.Windows.Forms.RadioButton rb_long;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ RToast/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RToast
+{
+    public partial class Form1 : Form
+    {
+
+        Toaster t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.comboBox1.Enabled = false;
+            this.panel1.Enabled = false;
+            this.panel2.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox1.Enabled = true;
+            this.panel1.Enabled = true;
+            this.panel2.Enabled = true;
+            this.button2.Enabled = true;
+            this.button3.Enabled = true;
+            t = new Toaster();
+            t.add_heading(textBox1.Text);
+            t.add_text(textBox2.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var check = this.panel2.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name;
+            switch (check.ToString())
+            {
+                case "rb_short":
+                    t.set_duration(Toaster.Duration.Short);
+                    break;
+                case "rb_long":
+                    t.set_duration(Toaster.Duration.Long);
+                    break;
+                case "rb_stay":
+                    t.set_duration(Toaster.Duration.Stay);
+                    break;
+            }
+            //Console.WriteLine(check.ToString());
+
+            t.show_toast();
+            this.panel1.Enabled = false;
+            this.panel2.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+            this.comboBox1.Enabled = false;
+            t = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            t.add_heading(textBox1.Text);
+            label6.Text = textBox1.Text.Length.ToString();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            t.add_text(textBox2.Text);
+            label7.Text = textBox2.Text.Length.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var check = this.panel2.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name;
+            switch (check.ToString())
+            {
+                case "rb_short":
+                    t.set_duration(Toaster.Duration.Short);
+                    break;
+                case "rb_long":
+                    t.set_duration(Toaster.Duration.Long);
+                    break;
+                case "rb_stay":
+                    t.set_duration(Toaster.Duration.Stay);
+                    break;
+            }
+            t.schedule_toast(DateTime.Now.AddSeconds(Convert.ToDouble(this.comboBox1.SelectedItem.ToString())));
+            //Console.WriteLine(this.comboBox1.SelectedItem.ToString());
+            //return;
+            this.panel1.Enabled = false;
+            this.panel2.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+            this.comboBox1.Enabled = false;
+            t = null;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
